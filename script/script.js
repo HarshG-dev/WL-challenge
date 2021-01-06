51cd2973ac4e56a558271bc475b53f4185f4d28e
HarshG-dev <72975106+HarshG-dev@users.noreply.github.com>
2021-01-06 21:50:47 +0000

    Correct indentation and minor JSHint fixes

--- script/script.js
+++ script/script.js
@@ -1,24 +1,24 @@
 
 window.onload = function () {
-"use strict";
+    "use strict";
 
-// Select all the accordion class element
-const SECTIONS = document.querySelectorAll(".accordion");
+    // Select all the accordion class element
+    const SECTIONS = document.querySelectorAll(".accordion");
 
-// Arrow function and add the toggle event listener
-SECTIONS.forEach(accordion => {
-    accordion.addEventListener("toggle", openOneSection)
-});
+    // Arrow function and add the toggle event listener
+    SECTIONS.forEach(accordion => {
+        accordion.addEventListener("toggle", openOneSection);
+    });
 
-// 'openOneSection' function to close the accordions which are 'open'
-function openOneSection() {
-    if (this.open) {
-        SECTIONS.forEach(accordion => {
-            if (accordion !== this && accordion.open) {
-                accordion.open = false;
-            }
-        });
+    // 'openOneSection' function to close the accordions which are 'open'
+    function openOneSection() {
+        if (this.open) {
+            SECTIONS.forEach(accordion => {
+                if (accordion !== this && accordion.open) {
+                    accordion.open = false;
+                }
+            });
+        }
     }
-}
 
-}
+};
